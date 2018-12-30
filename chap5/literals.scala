5236863b4608b7bd7d830e6ead99ee66a0e68a99
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-12-30 04:29:56 +0100

    Add literals example

---
+++ chap5/literals.scala
@@ -0,0 +1,44 @@
+#!/usr/bin/env scala
+
+
+// int / hex literals
+val hex = 0xcaffe
+val hex1 = 0xaffe
+val hex2 = 0xbeef
+val res = hex2 << 32 | hex1
+println(res)
+
+val ii: Int = 100
+
+// float literal
+val f = 100.10f
+
+// double literal
+val d = 10.10d
+
+// char literal
+val c = 'a' + 1
+println(c)
+
+// string literal
+val s = "Hello World"
+val ms = """
+Python-like Strings!
+"""
+println(ms)
+
+// long literal
+val l = 100l
+
+// short literal
+val i: Short = 100
+
+// unicode literal 
+val u = '\u0041'
+println(u)
+
+// symbol literal
+val sym: Symbol = 'aSymbol
+println(sym)
+println(sym.name)
+
