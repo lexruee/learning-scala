ff551a13415afc68595d04d2f50504ab9ac209c3
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-12-30 04:13:25 +0100

    Add basic types example

---
+++ chap5/basic-types.scala
@@ -0,0 +1,35 @@
+#!/usr/bin/env scala
+
+val f: Float = 40.5f
+val l: Long = 100000L
+val s: Short = 400
+val i: Int = 100
+val b: Byte = 0xf
+val c: Char = 'a'
+val d: Double = 100.3d
+val t: Boolean = false
+
+
+
+println("Int.MaxValue: " + Int.MaxValue)
+println("Int.MinValue: " + Int.MinValue)
+
+println("Short.MaxValue: " + Short.MaxValue)
+println("Short.MinValue: " + Short.MinValue)
+
+println("Byte.MaxValue: " + Byte.MaxValue)
+println("Byte.MinValue: " + Byte.MinValue)
+
+println("Char.MaxValue: " + Char.MaxValue)
+println("Char.MinValue: " + Char.MinValue)
+
+println("Long.MaxValue: " + Long.MaxValue)
+println("Long.MinValue: " + Long.MinValue)
+
+println("Double.MaxValue: " + Double.MaxValue)
+println("Double.MinValue: " + Double.MinValue)
+
+println("Float.MaxValue: " + Float.MaxValue)
+println("Float.MinValue: " + Float.MinValue)
+
+
