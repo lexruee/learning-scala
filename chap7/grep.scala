524daf9ca3dbed78a0028fcb5d71adbcd31ae308
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-01-01 04:35:59 +0100

    Add grep example

---
+++ chap7/grep.scala
@@ -0,0 +1,42 @@
+#!/usr/bin/env scala
+
+import java.io.File
+import scala.io.Source
+
+def showUsage { 
+  println("""
+    |Usage: grep.scala [-h | --help] PATTERN [directory]
+    |Lists the contents of the specified directory.""".stripMargin)
+}
+
+def fileLines(file: File): List[String] =  Source.fromFile(file).getLines.toList
+
+def ls(dir: String): List[File] = { 
+  (new File(dir))
+    .listFiles
+    .toList
+    .sortWith((f1, f2) => f1.getName <= f2.getName)
+}
+
+def grepFiles(files: List[File], pattern: String): List[String] = { 
+  for {
+    file <- files if file.isFile
+    line <- fileLines(file) 
+    if line.trim.matches(pattern)
+  } yield file + ": " + line.trim
+}
+
+if (List("--help", "-h").contains(args(0))) {
+  showUsage
+} else if (args.length >=1) {
+  val pattern = args(0)
+  val dir = if (args.length == 2) args(1) else "."
+  val files = ls(dir)
+  val result = grepFiles(files, pattern)
+  println(result.mkString("\n"))
+} else {
+  showUsage
+  System.exit(1)
+}
+
+
